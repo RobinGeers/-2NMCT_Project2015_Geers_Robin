a838918721d2f400eac8a9f7dbdcc1e19de46474
RobinGeers <robin.geers@student.howest.be>
2015-04-27 21:29:35 +0200

    Toon Lat en Long van gekozen Kot

--- app/src/main/java/nmct/howest/be/desproject/KotzoneDetailsFragment.java
+++ app/src/main/java/nmct/howest/be/desproject/KotzoneDetailsFragment.java
@@ -4,41 +4,25 @@ package nmct.howest.be.desproject;
 import android.app.ActionBar;
 import android.content.Intent;
 import android.location.Address;
-import android.location.Geocoder;
-import android.location.Location;
 import android.os.Bundle;
 import android.app.Fragment;
-import android.os.Handler;
-import android.os.ResultReceiver;
-import android.support.annotation.Nullable;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
-
-import java.io.IOException;
 import java.util.List;
-import java.util.Locale;
 
 public class KotzoneDetailsFragment extends Fragment {
 
-    private TextView textViewDetailsAdres, textViewDetailsStad, textViewDetailsPostcode;
+    private TextView textViewLatitude, textViewLongitude;
     private Button buttonBack;
     private List<Address> listAddressen;
 
-    public static final String EXTRA_MARKER_LATITUDE = "";
-    public static final String EXTRA_MARKER_LONGITUDE = "";
+    public static final String EXTRA_MARKER_LATITUDE = "latitude";
+    public static final String EXTRA_MARKER_LONGITUDE = "longitude";
     public static String EXTRA_GEKOZEN_KOTZONE;
-    public static double EXTRA_LATITUDE = 0;
-    public static double EXTRA_LONGITUDE = 0;
 
 
 
@@ -51,9 +35,9 @@ public class KotzoneDetailsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View viewKotzoneDetails = inflater.inflate(R.layout.fragment_kotzone_details, container, false);
 
-        textViewDetailsAdres = (TextView) viewKotzoneDetails.findViewById(R.id.textViewDetailsAdres);
-        textViewDetailsStad = (TextView) viewKotzoneDetails.findViewById(R.id.textViewDetailsStad);
-        textViewDetailsPostcode = (TextView) viewKotzoneDetails.findViewById(R.id.textViewDetailsPostcode);
+        textViewLatitude = (TextView) viewKotzoneDetails.findViewById(R.id.textViewDetailsLatitude);
+        textViewLongitude = (TextView) viewKotzoneDetails.findViewById(R.id.textViewDetailsLongitude);
+
         buttonBack = (Button) viewKotzoneDetails.findViewById(R.id.buttonBack);
 
         ActionBar ab = getActivity().getActionBar();
@@ -62,8 +46,8 @@ public class KotzoneDetailsFragment extends Fragment {
         if (getArguments() != null) {
             double latitude = getArguments().getDouble(EXTRA_MARKER_LATITUDE);
             double longitude = getArguments().getDouble(EXTRA_MARKER_LONGITUDE);
-            EXTRA_LATITUDE = latitude;
-            EXTRA_LONGITUDE = longitude;
+            textViewLatitude.setText("Latitude: " + latitude);
+            textViewLongitude.setText("Longitude: " + longitude);
 
             buttonBack.setOnClickListener(new View.OnClickListener() {
                 @Override
